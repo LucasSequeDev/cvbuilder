b922880dec51896509ad869a62351a0ad7719571
LucasSequeDev <lucas.sequeira@yahoo.com.ar>
2022-04-08 09:29:48 -0300

    first commit

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,26 @@
 const ORM = require('../functions/orm')
+const { validationResult } = require('express-validator')
 
 const controller ={
-    index:(req, res) => {
-        res.render('index', { msg: "Lienzo en blanco"})
+    renderCV:(req, res) => {
+        const id = req.params.id
+        const data = ORM.findAll( {collection: 'cv'} )
+        const user = data.filter( item => item.id === id)
+        
+        if( user.length === 0 ) {
+            return res.render('404')
+        }
+        
+        return res.render('cv', user[0] )
     },
+    login: (req, res) => {
+        res.render('login')
+    },
+    auth: (req, res) => {
+        const resultado = validationResult(req)
+        const errors = resultado.mapped()
+        res.render('login', { errors })
+    }
 }
 
 module.exports = controller;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const controller = require("../controllers/");
+const { body } = require("express-validator")
 
 const router = express.Router();
 
-router.get('/', controller.index)
+const validatorPostLogin = [
+    body('email').isEmail().notEmpty().withMessage('mensaje requerido')
+]
+
+router.get('/login', controller.login)
+router.post('/login', validatorPostLogin ,controller.auth)
+
+router.get('/:id', controller.renderCV)
 
 module.exports = router;
